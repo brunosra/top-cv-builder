e73aa9888e80409dc81e08d086efd7f8473a6cdd
Bruno Almeida <brunosra@gmail.com>
2023-08-13 15:21:30 +0100

    Add Textfield

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Resume from "./Resume";
+import TextField from "./components/TextField";
 import "./App.css";
 
 function App() {
@@ -7,7 +8,25 @@ function App() {
 
   return (
     <>
-      <Resume />
+      <div className="forms-sidebar">
+        <div className="card">
+          <div className="header">
+            <h2>Personal Information</h2>
+            <span className="material-symbols-outlined">
+              expand_more expand_less
+            </span>
+          </div>
+          <div className="body">
+            <TextField value="test" />
+          </div>
+        </div>
+      </div>
+      <Resume
+        name="John Smith"
+        email="john@smith.com"
+        phone="+1 55 555 55 55"
+        location="Narnia, USA"
+      />
     </>
   );
 }
---
+++ src/components/TextField.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./TextField.css";
+
+function TextInput({
+  name,
+  label,
+  initialValue = "",
+  type = "text",
+  handleChange,
+  ...props
+}) {
+  const [value, setValue] = useState(initialValue);
+
+  return (
+    <div className="fieldGroup">
+      <label htmlFor={name}>{label}</label>
+      <br />
+      <input
+        {...props}
+        name={name}
+        type={type}
+        value={initialValue}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default TextInput;
