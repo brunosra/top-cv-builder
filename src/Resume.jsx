96158a324ca30083b9d3b37b58e21150255803ab
Bruno Almeida <brunosra@gmail.com>
2023-08-15 17:25:08 +0100

    Uplift state

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,6 +1,8 @@
 import "./Resume.css";
 import { v4 as uuidv4 } from "uuid";
 import EditButton from "./components/EditButton";
+import { useState } from "react";
+import PersonalFields from "./components/PersonalFields";
 
 let educationDefaultArr = [
   {
@@ -55,61 +57,89 @@ function Resume({
   education = educationDefaultArr,
   experience = experienceDefaultArr,
 }) {
+  const [mode, setMode] = useState("view");
+  const [data, setData] = useState({});
+
+  const openPersonalForm = () => {
+    setMode("editPersonal");
+  };
+
+  const cancelForm = () => {
+    setMode("view");
+  };
+
+  const captureDataFromTextFields = (e) => {
+    console.log(e.target.name, e.target.value);
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className="page resume">
-      <div className="header-resume personal-info">
-        <EditButton handleClick={() => console.log("click!")} />
-        <h1>{name}</h1>
-        {email && (
-          <span className="info">
-            <span className="material-symbols-outlined">mail</span> {email}
-          </span>
-        )}
-        {phone && (
-          <span className="info">
-            <span className="material-symbols-outlined">phone_in_talk</span>{" "}
-            {phone}
-          </span>
-        )}
-        {location && (
-          <span className="info">
-            <span className="material-symbols-outlined">location_on</span>{" "}
-            {location}
-          </span>
-        )}
+    <>
+      {mode == "editPersonal" ? (
+        <PersonalFields
+          handleTextFieldChange={captureDataFromTextFields}
+          handleSubmit={captureDataFromTextFields}
+          handleCancel={cancelForm}
+          id="personal"
+          data={data}
+        />
+      ) : null}
+
+      <div className="page resume">
+        <div className="header-resume personal-info">
+          <EditButton handleClick={openPersonalForm} />
+          <h1>{name}</h1>
+          {email && (
+            <span className="info">
+              <span className="material-symbols-outlined">mail</span> {email}
+            </span>
+          )}
+          {phone && (
+            <span className="info">
+              <span className="material-symbols-outlined">phone_in_talk</span>{" "}
+              {phone}
+            </span>
+          )}
+          {location && (
+            <span className="info">
+              <span className="material-symbols-outlined">location_on</span>{" "}
+              {location}
+            </span>
+          )}
+        </div>
+        <div className="section-resume education">
+          <EditButton handleClick={() => console.log("click!")} />
+          <h2>Education</h2>
+          <ul>
+            {education.map((item) => (
+              <li key={item.uuid}>
+                <h3>{item.degree}</h3>
+                <p>{item.institution}</p>
+                <p>{item.date}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="section-resume experience">
+          <EditButton handleClick={() => console.log("click!")} />
+          <h2>Latest Experience</h2>
+          <ul>
+            {experience.map((item) => (
+              <li key={item.uuid}>
+                <h3>{item.position}</h3>
+                <h4>
+                  {item.company} ·{" "}
+                  <span className="date">
+                    {item.fromDate} – {item.toDate}
+                  </span>
+                </h4>
+                <p>{item.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="section-resume education">
-        <EditButton handleClick={() => console.log("click!")} />
-        <h2>Education</h2>
-        <ul>
-          {education.map((item) => (
-            <li key={item.uuid}>
-              <h3>{item.degree}</h3>
-              <p>{item.institution}</p>
-              <p>{item.date}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="section-resume experience">
-        <EditButton handleClick={() => console.log("click!")} />
-        <h2>Latest Experience</h2>
-        <ul>
-          {experience.map((item) => (
-            <li key={item.uuid}>
-              <h3>{item.position}</h3>
-              <h4>
-                {item.company} ·{" "}
-                <span className="date">
-                  {item.fromDate} – {item.toDate}
-                </span>
-              </h4>
-              <p>{item.description}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </>
   );
 }
 
